37860e8c6546e3a6e1b0284592c609d34bae1412
David Niederweis <david.niederweis@gmail.com>
2022-12-02 11:45:46 -0600

    fix typo, make day1 run regardless of current directory

--- src/day1.rs
+++ src/day1.rs
@@ -1,29 +1,30 @@
 use std::fs;
 
-pub fn get_list_elfs() -> Vec<i32> {
-    let contents = fs::read_to_string("day1_input.txt").expect("Can't read file");
+pub fn get_list_elves() -> Vec<i32> {
+    let path = concat!(env!("CARGO_MANIFEST_DIR"), "/src/day1_input.txt");
+    let contents = fs::read_to_string(path).expect("Can't read file");
     let lines: Vec<&str> = contents.split("\n").collect();
-    let mut elfs: Vec<i32> = Vec::new();
+    let mut elves: Vec<i32> = Vec::new();
     let mut total = 0;
     for line in &lines {
         if !line.is_empty() {
             total += line.parse::<i32>().unwrap();
         } else {
-            elfs.push(total);
+            elves.push(total);
             total = 0;
         }
     }
-    elfs.sort();
-    elfs
+    elves.sort();
+    elves
 }
 
 pub fn solve_part1() -> i32 {
-    let elfs = get_list_elfs();
-    elfs.last().unwrap().clone()
+    let elves = get_list_elves();
+    elves.last().unwrap().clone()
 }
 
 pub fn solve_part2() -> i32 {
-    let elfs = get_list_elfs();
-    elfs.iter().rev().take(3).sum()
+    let elves = get_list_elves();
+    elves.iter().rev().take(3).sum()
 }
 
